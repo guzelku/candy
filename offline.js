0b0c4116aa87cf84f6e67d2cc92a0c1de0a357d0
guzelku <62844142+guzelku@users.noreply.github.com>
2023-11-04 15:24:52 +0500

    1
    1

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1699093318,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ПлиточныйФон.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт-sheet1.png",
+		"images/конфета-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Частицы.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
